3ee1e2eb010ec27cfc36443041aeb4fbc1d63886
Andy DeLeon <agdl2003@gmail.com>
2020-10-01 15:23:55 -0700

    worked on the main body and all that is missing is the files section and the js

---
+++ checklist.js
@@ -0,0 +1,18 @@
+var progressAmount = document.getElementById('progress-bar')
+var checkCount = 0;
+var task1 = document.getElementById('task-1');
+var task2 = document.getElementById('task-2');
+var task3 = document.getElementById('task-3');
+var task4 = document.getElementById('task-4');
+
+function progressBar(){
+  if(task1.checked===true){
+
+  }
+
+  //this if statement adds a percentage to the progress bar
+  if(checkCount==1)
+}
+progressBar()
+setInterval(progressBar(), 1);
+alert('remember to move the css so that its not external')
