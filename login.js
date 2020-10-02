4713f127d0e6dde5a721bb3564c3578acb4a2be5
Caleb Sonnichsen <csonnichsen186@west-mec.org>
2020-10-02 15:28:07 -0700

    Updated Login.html and added to login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+var user = "Cade123";
+var pass = "Cade123";
+var enteredUser=document.getElementsByTagName('input')[0].value;
+var enteredPass=document.getElementsByTagName('input')[1].value;
+function LoginISB(){
+    if(user===enteredUser&&pass===enteredPass)
+    {
+        console.log("Login")
+    }
+    else{
+    }
+}
